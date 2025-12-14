a6753c885c50c451c5fc5ef74c51d77eb4cbbe60
akhatre <akhatre@users.noreply.github.com>
2025-12-14 14:20:30 +0000

    add patched otel openai plugin

--- overmind/__init__.py
+++ overmind/__init__.py
@@ -2,11 +2,12 @@
 Overmind Python Client
 
 A Python client for the Overmind API that provides easy access to AI provider endpoints
-with policy enforcement.
+with policy enforcement and automatic observability.
 """
 
 from .client import OvermindClient
 from .exceptions import OvermindAPIError, OvermindAuthenticationError, OvermindError
+from .overmind_sdk import init, get_tracer, set_user, set_tag, capture_exception
 
 __version__ = "0.1.0"
 __all__ = [
@@ -14,4 +15,9 @@ __all__ = [
     "OvermindError",
     "OvermindAPIError",
     "OvermindAuthenticationError",
+    "init",
+    "get_tracer",
+    "set_user",
+    "set_tag",
+    "capture_exception",
 ]
